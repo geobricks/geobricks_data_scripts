2d35b413e98df0d7ac3c24b08c7a9c7f4c9bf9a4
vortex <simone.murzilli@fao.org>
2014-12-22 13:25:51 +0100

    update

--- geobricks_data_scripts/demo/__init__.py
+++ geobricks_data_scripts/demo/__init__.py
@@ -1,1 +1,4 @@
-__author__ = 'vortex'
+_package__ = 'geobricks_data_scripts'
+__author__ = 'Barbaglia, Guido - Murzilli, Simone'
+__email__ = 'guido.barbaglia@gmail.com; simone.murzilli@gmail.com;'
+__license__ = 'GPL2'
