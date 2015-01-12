241cd9e49fe4240cffadfeb1064b9ef5569e0cf4
vortex <simone.murzilli@fao.org>
2015-01-12 17:14:05 +0100

    updated metadata

--- geobricks_data_scripts/demo/ghg/data/update/update_metadata.py
+++ geobricks_data_scripts/demo/ghg/data/update/update_metadata.py
@@ -6,21 +6,20 @@ data_manager = get_data_manager()
 # Update the dsd structure adding changing the wms_url and the datasource
 def update_dsd_with_wms_and_datasource(wms_url,  datasource):
 
-    layers = data_manager.metadata_manager.get_all_layers()
+    layers = data_manager.metadata_manager.get_all_layers(False, True)
     print len(layers)
     for l in layers:
         dsd = l["dsd"]
         dsd["datasource"] = datasource
         print l["uid"], dsd
-        # result = data_manager.metadata_manager.overwrite_dsd_rid(dsd)
-        # print result
-        # update_dsd_with_wms_and_datasource_by_uid(l["uid"], wms_url,  datasource)
+        #update_dsd_with_wms_and_datasource_by_uid(l["uid"], wms_url,  datasource)
 
 
 def update_dsd_with_wms_and_datasource_by_uid(uid, wms_url,  datasource):
-    layer = data_manager.metadata_manager.get_by_uid(uid)
+    layer = data_manager.metadata_manager.get_by_uid(uid, False, True)
     dsd = layer["dsd"]
     dsd["datasource"] = datasource
+    #print dsd
     result = data_manager.metadata_manager.overwrite_dsd_rid(dsd)
     print result
 
