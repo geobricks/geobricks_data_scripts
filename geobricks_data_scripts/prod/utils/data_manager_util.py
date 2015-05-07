7b613e5f35055fb1865b401b5ff6ec8a43cb3928
simonefenix <simone.murzilli@fao.org>
2015-05-07 17:49:43 +0200

    updated script

--- geobricks_data_scripts/prod/utils/data_manager_util.py
+++ geobricks_data_scripts/prod/utils/data_manager_util.py
@@ -3,4 +3,6 @@ from geobricks_data_scripts.prod.config.config import settings
 
  # get the default data manager for demo db
 def get_data_manager():
+    print "Prod DAata Manager"
+    print settings
     return DataManager(settings)
