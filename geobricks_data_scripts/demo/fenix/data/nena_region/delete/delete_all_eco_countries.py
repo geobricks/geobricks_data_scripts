f0921e61b6e75c1f02b0db321f7a810dbd24563d
stefanocudini <stefano.cudini@gmail.com>
2015-10-21 11:06:33 +0200

    added scripts for nena

---
+++ geobricks_data_scripts/demo/fenix/data/nena_region/delete/delete_all_eco_countries.py
@@ -0,0 +1,14 @@
+from geobricks_data_scripts.demo.fenix.utils.data_manager_util import get_data_manager
+
+
+data_manager = get_data_manager()
+layers = data_manager.get_all_layers()
+print "layers"
+for layer in layers:
+    try:
+        if "nena_" in layer["dsd"]["workspace"]:
+            print layer["uid"]
+            data_manager.delete(layer["uid"])
+    except Exception, e:
+        #print "Error", e
+        pass
